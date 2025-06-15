a9205bb6031742cf98ef4b4f6b74cc4b63856630
Robin Heinz <robin-heinz@web.de>
2025-06-15 12:10:45 +0200

    makes staff list use generic navigation list

--- apps/dashboard/src/routes/dashboard/staff/components/navigation/StaffList.tsx
+++ apps/dashboard/src/routes/dashboard/staff/components/navigation/StaffList.tsx
@@ -1,42 +1,28 @@
-import { List, ListItem, ListItemButton, Typography } from '@mui/material';
-import { NavLink } from 'react-router-dom';
-
-import { SideNavSection, FallbackMessage } from '@studiobooker/utils';
+import {
+  SideNavSection,
+  NavigationList,
+  useNumericParam,
+} from '@studiobooker/utils';
 import { useAllStaff } from '../../../../../hooks/staff.queries';
 import AddStaff from './AddStaff';
-import StaffListContentSkeleton from './StaffListContentSkeleton';
 
 export default function StaffList() {
-  const { isPending, staff, isError } = useAllStaff();
+  const { isLoading, staff, isError } = useAllStaff();
 
-  const content = isError ? (
-    <FallbackMessage
-      variant="body1"
-      marginTop={0}
-      message="Something went wrong..."
-    />
-  ) : (
-    <List disablePadding sx={{ flex: 1, overflowY: 'auto' }}>
-      {isPending && <StaffListContentSkeleton />}
-      {staff?.map((s) => (
-        <ListItemButton key={s.id}>
-          <ListItem
-            component={NavLink}
-            to={s.id.toString()}
-            sx={{
-              color: 'inherit',
-            }}
-          >
-            <Typography noWrap>{s.name}</Typography>
-          </ListItem>
-        </ListItemButton>
-      ))}
-    </List>
-  );
+  const staffId = useNumericParam('id');
 
   return (
     <SideNavSection title="Staff" actionComponent={<AddStaff />}>
-      {content}
+      <NavigationList
+        isLoading={isLoading}
+        isError={isError}
+        data={staff}
+        labelProperty="name"
+        errorMessage="Something went wrong. :("
+        fallbackMessage="No staff yet."
+        isActive={(staffMember) => staffMember.id === staffId}
+        navigateTo={(staffMember) => staffMember.id.toString()}
+      />
     </SideNavSection>
   );
 }
