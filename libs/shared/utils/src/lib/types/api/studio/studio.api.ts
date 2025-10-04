12f4838794250d0e463d7a221d48025839aae69a
Robin Heinz <robin-heinz@web.de>
2025-10-04 10:19:47 +0200

    fix studio api type error

--- libs/shared/utils/src/lib/types/api/studio/studio.api.ts
+++ libs/shared/utils/src/lib/types/api/studio/studio.api.ts
@@ -1,6 +1,7 @@
 import { ApiServiceCategoryStructured } from '../service-category/service-category.api';
 
 export type ApiStudioInformation = {
+  companyId: number;
   name: string;
   description: string;
   services: ApiServiceCategoryStructured[];
