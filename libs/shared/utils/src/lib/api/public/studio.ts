03df1677f0c2e7eec06cba4fc4c1ae82c31e9df1
Robin Heinz <robin-heinz@web.de>
2025-10-04 15:55:06 +0200

    fourth try

--- libs/shared/utils/src/lib/api/public/studio.ts
+++ libs/shared/utils/src/lib/api/public/studio.ts
@@ -12,12 +12,14 @@ export async function getStudioPublic(studioId: string) {
       `public/studio/${studioId}`,
       200
     );
+    console.warn(data)
 
     return mapApiToStudioInformation(data);
   } catch (error) {
     if (error instanceof QueryError && error.status === 404) {
       notFound();
     } else {
+      console.error('real error', error)
       throw error;
     }
   }
